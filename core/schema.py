5a22f597fe83250a3117c2a7d5f3ea596b707106
Luca Rose <63530231+lucacrose@users.noreply.github.com>
2026-01-02 03:25:56 -0600

    Implemented resolver

--- core/schema.py
+++ core/schema.py
@@ -1,8 +1,24 @@
-from dataclasses import dataclass
-from typing import Tuple, Optional
+from dataclasses import dataclass, field
+from typing import List, Tuple, Optional
 
 @dataclass
 class Box:
     coords: Tuple[int, int, int, int]
     label: str
     confidence: float
+
+@dataclass
+class ResolvedItem:
+    name: str = "Unknown"
+    value: int = 0
+    container_box: Optional[Box] = None
+    thumb_box: Optional[Box] = None
+    name_box: Optional[Box] = None
+    value_box: Optional[Box] = None
+
+@dataclass
+class TradeLayout:
+    outgoing_items: List[ResolvedItem] = field(default_factory=list)
+    incoming_items: List[ResolvedItem] = field(default_factory=list)
+    incoming_robux_box: int = 0
+    outgoing_robux_box: int = 0
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from core.detector import TradeDetector
+from core.resolver import SpatialResolver
 
 detector = TradeDetector("assets/weights/yolo_v1.pt")
+resolver = SpatialResolver()
 
 boxes = detector.detect("test.png")
+trade_layout = resolver.resolve(boxes)
 
 print(boxes)
+print(trade_layout)
