90572b1221cf1f4297baaa4c03e74dbae0be52d4
Luca Rose <63530231+lucacrose@users.noreply.github.com>
2026-01-02 04:02:44 -0600

    Implemented OCR

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ reader = OCRReader()
 boxes = detector.detect("test.png")
 trade_layout = resolver.resolve(boxes)
 
-
 print(boxes)
+print(trade_layout)
+
+reader.process_layout("test.png", trade_layout)
+
 print(trade_layout)
